125da0498b014b0d64dc00a4415f5afa895bad36
Yogesh Datir <yogdat@gmail.com>
2023-02-18 02:37:40 +0530

    Added UI for upvotes, comments on list, view pages for desktop

---
+++ client/src/pages/home/FeedbackList/FeedbackCard.tsx
@@ -0,0 +1,50 @@
+import React, { MouseEvent } from "react";
+import { ViewBadge } from "../../../components/Common.styled";
+import {
+  Upvotes,
+  FeedbackContentWrapper,
+  FeedbackTitle,
+  FeedbackDescription,
+  CommentCountWrapper,
+  CommentCount,
+  StyledFeedbackCard,
+} from "./FeedbackList.styled";
+import { IFeedback } from "../../../utils/types";
+import { ReactComponent as UpArrow } from "../../../assets/shared/icon-arrow-up.svg";
+import { ReactComponent as CommentsIcon } from "../../../assets/shared/icon-comments.svg";
+
+interface Props {
+  feedback: IFeedback;
+  onClick?: () => void;
+  isForView?: boolean;
+}
+
+function FeedbackCard({ feedback, onClick, isForView }: Props) {
+  return (
+    <StyledFeedbackCard onClick={onClick} isForView={isForView}>
+      <Upvotes
+        onClick={(e: MouseEvent<HTMLButtonElement>) => {
+          e.stopPropagation();
+        }}
+      >
+        <UpArrow />
+        <span>155</span>
+      </Upvotes>
+      <FeedbackContentWrapper>
+        <FeedbackTitle className="feedback-title">
+          {feedback.title}
+        </FeedbackTitle>
+        <FeedbackDescription style={{ marginTop: "0.25rem" }}>
+          {feedback.description}
+        </FeedbackDescription>
+        <ViewBadge>{feedback.category}</ViewBadge>
+      </FeedbackContentWrapper>
+      <CommentCountWrapper>
+        <CommentsIcon />
+        <CommentCount>1</CommentCount>
+      </CommentCountWrapper>
+    </StyledFeedbackCard>
+  );
+}
+
+export default FeedbackCard;
