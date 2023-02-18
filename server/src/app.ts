0326c7e77a1d77ee9a6da1173fe03c44a82dbced
Yogesh Datir <yogdat@gmail.com>
2023-02-18 18:54:55 +0530

    added user object in app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,18 @@
-import express from "express";
-import feedbackRouter from "./routes/feedbackRoutes";
-import * as dotenv from "dotenv";
-import { Pool } from "pg";
-import statusRouter from "./routes/statusRoutes";
-import categoryRouter from "./routes/categoryRoutes";
-import cors from "cors";
+import express from 'express';
+import feedbackRouter from './routes/feedbackRoutes';
+import * as dotenv from 'dotenv';
+import { Pool } from 'pg';
+import statusRouter from './routes/statusRoutes';
+import categoryRouter from './routes/categoryRoutes';
+import cors from 'cors';
+import userRouter from './routes/userRoutes';
 
 dotenv.config();
 
 const DEV_DB_HOST = process.env.DEV_DB_HOST;
 const DEV_DB_USERNAME = process.env.DEV_DB_USERNAME;
 const DEV_DB_PORT = Number(process.env.DEV_DB_PORT);
-const DEV_DB_PASSWORD = process.env.DEV_DB_PASSWORD || "";
+const DEV_DB_PASSWORD = process.env.DEV_DB_PASSWORD || '';
 const DEV_DB_NAME = process.env.DEV_DB_NAME;
 
 export const pool = new Pool({
@@ -30,9 +31,10 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/feedbacks", feedbackRouter);
-app.use("/status", statusRouter);
-app.use("/categories", categoryRouter);
+app.use('/feedbacks', feedbackRouter);
+app.use('/status', statusRouter);
+app.use('/categories', categoryRouter);
+app.use('/users', userRouter);
 
 app.listen(PORT, () =>
   console.log(`server running at http://localhost:${PORT}`)
